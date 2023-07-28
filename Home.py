62d7ce5b4c0e8aa254c6e7cca965f66e1b953f33
James Gill <gilford88@gmail.com>
2023-07-28 11:06:24 +0100

    added contact us page and side navigation bar

--- Home.py
+++ Home.py
@@ -6,7 +6,7 @@ st.set_page_config(layout='wide')
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image('images/photo.png', width=600)
+    st.image('images/photo.png')
 
 with col2:
     st.title('James Gill')
@@ -18,7 +18,7 @@ with col2:
     """
     st.info(content)
 
-st.write("Below you can find some of the apps i have built in Python, Feel free to contact me!")
+st.subheader("Below you can find some of the apps i have built in Python, Feel free to contact me!")
 
 col3, colgap, col4 = st.columns([1.5, 0.5, 1.5])
 
---
+++ pages/Contact_Us.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.header('Contact Me')
+
+st.form(key='Info')
