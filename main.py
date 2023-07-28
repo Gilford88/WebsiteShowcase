3a26bd17f425f3049d64d8751dffd9a0e1c16b27
James Gill <gilford88@gmail.com>
2023-07-28 09:39:34 +0100

    Add photo and description to the new columns

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-import PysimpleGUI as sg
+import streamlit as st
 
+st.set_page_config(layout='wide')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image('images/photo.png', width=600)
+
+with col2:
+    st.title('James Gill')
+    content = """I have started on my journey to becomes a software developer. I have created this showcase page to
+    demonstrate my knowledge in Python amongst other skills. I graduated with a degree in Business Management in 
+    2010 and entered a career in sales, but i found that this is not a subject that is testing and i found myself 
+    seeking a new challenge. I am looking forward to moving into a career in tech and am looking at progressing down
+    the DevOps route. 
+    """
+    st.info(content)
+
+st.write("Below you can find some of the apps i have built in Python, Feel free to contact me!", font=30)
