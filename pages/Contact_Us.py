40aa5c36e61fd0eb219431848a4e92596d4cd4ae
James Gill <gilford88@gmail.com>
2023-07-28 11:39:25 +0100

    day 23: Setup email sending from contact form

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import send_email
 
 st.header('Contact Me')
 
-st.form(key='Info')
+with st.form(key='contact_form'):
+    user_email = st.text_input("Your email address")
+    user_message = st.text_area("Your Message")
+    button = st.form_submit_button("Submit")
+    if button:
+
